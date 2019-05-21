e927ac69a2a71c16bed0da58d3dc1ed1268badfe
Dorian Designs <aherasm@externos.bankia.com>
2019-05-21 10:11:29 +0200

    Webpack modularizado

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const autoprefixer = require('autoprefixer')
+
+const jsConfig = require('./webpack/js.config')
+const pugConfig = require('./webpack/pug.config')
+const cssSassConfig = require('./webpack/css-sass.config')
+const imagesConfig = require('./webpack/images.congif')
+const otherConfig = require('./webpack/other.config')
+
+const rules = [jsConfig, pugConfig, cssSassConfig, imagesConfig, otherConfig]
+
 
 module.exports = {
     devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.pug$/,
-                use: [
-                    {
-                        loader: 'pug-loader',
-                        options: { minimize: true }
-                    }
-                ]
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            autoprefixer: {
-                                browser: ['last 3 versions']
-                            },
-                            sourceMap: true,
-                            plugins: () => [autoprefixer]
-                        }
-                    },
-                    'resolve-url-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            outputStyle: 'compact',
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|svg|gif|webp)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name]-[hash].[ext]',
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(ttf|eot|woff2?|mp4|mp3|txt|xml|pdf)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name]-[hash].[ext]',
-                        }
-                    }
-                ]
-            }
-        ]
-    },
+    module: { rules },
     plugins: [
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin(),
@@ -110,6 +21,5 @@ module.exports = {
             template: './src/views/index.pug',
             file: './index.html'
         })
-
     ]
 }
