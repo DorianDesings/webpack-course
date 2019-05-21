55ec9f0cc1fd58236fe672b620b261fc713b4266
Dorian Designs <aherasm@externos.bankia.com>
2019-05-21 10:25:33 +0200

    añadido archivos json

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const jsConfig = require('./webpack/js.config')
-const pugConfig = require('./webpack/pug.config')
-const cssSassConfig = require('./webpack/css-sass.config')
-const imagesConfig = require('./webpack/images.congif')
-const otherConfig = require('./webpack/other.config')
+const jsConfig = require('./webpack-config/js.config')
+const pugConfig = require('./webpack-config/pug.config')
+const cssSassConfig = require('./webpack-config/css-sass.config')
+const imagesConfig = require('./webpack-config/images.congif')
+const jsonConfig = require('./webpack-config/json.config')
+const otherConfig = require('./webpack-config/other.config')
 
-const rules = [jsConfig, pugConfig, cssSassConfig, imagesConfig, otherConfig]
+const rules = [jsConfig, pugConfig, cssSassConfig, imagesConfig, jsonConfig, otherConfig]
 
 module.exports = {
     devtool: 'source-map',
